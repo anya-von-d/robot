fedfc298c816051aa1750e54e190b0675524dd5f
Anya Von D <anya.computer.science@gmail.com>
2025-12-05 04:25:33 +0000

    Add HW1

---
+++ autonomy_repo/scripts/heading_controller.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+import numpy as np
+import rclpy
+
+from asl_tb3_lib.control import BaseHeadingController
+from asl_tb3_lib.math_utils import wrap_angle
+from asl_tb3_msgs.msg import TurtleBotState, TurtleBotControl
+
+class HeadingController(BaseHeadingController):
+    def __init__(self):
+        super().__init__("heading_controller")
+        self.kp: float = 2.0
+    def compute_control_with_goal(self,state: TurtleBotState,goal: TurtleBotState) :
+        err = wrap_angle(goal.theta - state.theta
+        cmd = TurtleBotControl()
+        cmd.omega = float(self.kp * err)
+        return cmd
+def main() :
+    rclpy.init()
+    node = HeadingController()
+    try:
+        rclpy.spin(node)
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
+if __name__ == "__main__":
+    main()
+
